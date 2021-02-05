5253b36884c51eb1d7d841682f20c174ce56b66c
Cale Cortney <thortney@mailbox.org>
2021-02-05 12:58:20 -0600

    moved all projects to home page. link all host urls and repo urls. redesigned home layout. added mobile responsiveness.

---
+++ src/app/data/data.ts
@@ -0,0 +1,36 @@
+export const PROJECTS = [
+    {
+      title: "Husqr",
+      description: "Social media application utilizing a Firebase backend and an Angular front-end.",
+      url: "https://husqr-1074b.web.app",
+      repo: "https://github.com/calejc/husqr",
+      stack: ["Angular", "Firebase", "Bootstrap", "Angular-Material"]
+    },
+    {
+      title: "Movie Database",
+      description: "Java Spring Boot application to browse movies and their cast, actors and their credits, and more.",
+      url: "https://mcc-movies.herokuapp.com/about",
+      repo: "https://github.com/calejc/movies-project",
+      stack: ["Java", "Spring Boot", "Thymeleaf", "Bootstrap", "H2 Database"]
+    },
+    {
+        title: "Blackjack",
+        description: "A command line game of Blackjack.",
+        repo: "https://github.com/calejc/blackjack_cli",
+        stack: ["Java"]
+    },
+    {
+      title: "Rock, Paper, Scissors",
+      description: "Static version of the classic game, Rock, Paper, Scissors, using vanilla Javascript.",
+      url: "https://calejc-rps.herokuapp.com/",
+      repo: "https://github.com/calejc/rps",
+      stack: ["HTML", "CSS", "Javascript"]
+    },
+    {
+      title: "Integer Flashcards",
+      description: "Practice arithmetic flashcards with two different options for students to choose from. Designed to be embedded into a Google Site, using the Google Apps Script.",
+      url: "https://script.google.com/macros/s/AKfycbyyw602VxazPbrCBfjoi6c3jQEYlS3CinEQxuK6HF6bf4IGtbaq/exec",
+      repo: "https://github.com/calejc/flashcards",
+      stack: ["HTML", "CSS", "Javascript", "Google App Script"]
+    },
+  ]
