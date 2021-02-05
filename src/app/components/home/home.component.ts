83bf2becc190278444dd5565f94a46ac8a139ea4
Cale Cortney <thortney@mailbox.org>
2021-02-05 12:53:47 -0600

    moved all projects to home page. link all host urls and repo urls. redesigned home layout. added mobile responsiveness.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PROJECTS } from 'src/app/data/data';
+import { Project } from 'src/app/data/types/project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[] = PROJECTS
+
+  constructor() { 
+  }
+
+  getProjects(){
+    return this.projects;
+  }
 
   ngOnInit(): void {
   }
